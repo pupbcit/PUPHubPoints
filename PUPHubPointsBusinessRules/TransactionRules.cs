09cc11b594fa6b878c891c0f2108b98dfdbd41dd
indaleen <inquinsayas@gmail.com>
2023-06-27 18:19:53 +0800

    Added classes for saving and getting Points Transaction History

---
+++ PUPHubPointsBusinessRules/TransactionRules.cs
@@ -0,0 +1,44 @@
+﻿using PUPHubModels;
+using PUPHubPointsModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUPHubPointsBusinessRules
+{
+    public class TransactionRules
+    {
+        public TransactionRules()
+        {
+
+        }
+
+        public void SaveStudentPointsTransaction(StudentPointsTransaction studentPointsTransaction)
+        {
+
+        }
+
+        public List<StudentPointsTransaction> GetAllStudentPointsTransactions()
+        {
+            return null;
+        }
+
+        public List<StudentPointsTransaction> GetAllStudentPointsTransaction(Student student)
+        {
+            return null;
+        }
+
+        public List<StudentPointsTransaction> GetAllStudentPointsTransaction(Student student, DateTime transactionDateFrom, DateTime transactionDateTo)
+        {
+            return null;
+        }
+
+        public List<StudentPointsTransaction> GetAllStudentPointsTransaction(Student student, TransactionType transactionType)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ PUPHubPointsModels/StudentPointsTransaction.cs
@@ -0,0 +1,17 @@
+﻿using PUPHubModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUPHubPointsModels
+{
+    public class StudentPointsTransaction
+    {
+        public StudentPoint AvailableStudentPoints { get; set; }
+        public DateTime TransactionDateTime { get; set; }
+
+        public TransactionType TransactionType { get; set; }
+    }
+}
